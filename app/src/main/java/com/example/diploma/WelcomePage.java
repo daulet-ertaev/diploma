7986393fa30b064a0a03938f7c06302a2f46515c
hoffa017 <76833638+hoffa017@users.noreply.github.com>
2021-01-19 15:36:38 +0600

    welcomepage changed

--- app/src/main/java/com/example/diploma/WelcomePage.java
+++ app/src/main/java/com/example/diploma/WelcomePage.java
@@ -2,13 +2,35 @@ package com.example.diploma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomePage extends AppCompatActivity {
+
+
+    private Button btn_signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+
+
+        btn_signup = findViewById(R.id.btn_signup);
+        btn_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegister();
+            }
+        });
     }
+
+
+    public void openRegister() {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+    }
+
 }
