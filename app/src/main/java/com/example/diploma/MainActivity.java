e57a04da1eda0da98ef1a5b10a27c279c59aba1b
Alibi Nassipkali <nassipkali.alibi@gmail.com>
2021-01-19 14:26:18 +0500

    Added Login (Verify now надо исправить)

--- app/src/main/java/com/example/diploma/MainActivity.java
+++ app/src/main/java/com/example/diploma/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.diploma;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button resendCode;
+    TextView verifyMsg;
+    FirebaseAuth fAuth;
+    String userID;
+    private static final String TAG = "MainActivity";
 
     private ChipNavigationBar chipNavigationBar;
     private Fragment fragment=null;
@@ -18,6 +33,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        fAuth = FirebaseAuth.getInstance();
+
+        resendCode=findViewById(R.id.verifyButton);
+        verifyMsg=findViewById(R.id.verificationText);
+
+        userID = fAuth.getCurrentUser().getUid();
+        FirebaseUser user = fAuth.getCurrentUser();
+
+        if(!user.isEmailVerified()){
+            verifyMsg.setVisibility(View.VISIBLE);
+            resendCode.setVisibility(View.VISIBLE);
+
+            resendCode.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //
+                    user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(v.getContext(), "Verification Email has been sent.",Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "Failure! Email not sent. "+e.getMessage());
+                        }
+                    });
+                }
+            });
+        }
 
         chipNavigationBar=findViewById(R.id.chipNavigation);
         chipNavigationBar.setItemSelected(R.id.home, true);
@@ -45,5 +90,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
          });
+    }
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
