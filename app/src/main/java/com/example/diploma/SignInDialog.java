8b2c5c9648da5b749ae21fe40fbf02b9c4d47a6f
daulet-ertaev <65158013+daulet-ertaev@users.noreply.github.com>
2021-01-21 00:09:00 +0600

    Dialogues2. Log in ne rabotaet! Dialog doesnt reloads

--- app/src/main/java/com/example/diploma/SignInDialog.java
+++ app/src/main/java/com/example/diploma/SignInDialog.java
@@ -1,17 +1,31 @@
 package com.example.diploma;
 import android.app.Dialog;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.DialogFragment;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignInDialog extends DialogFragment {
+    private FirebaseAuth fAuth;
+    private EditText emailText, passwordText;
+    private TextView textView;
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -21,14 +35,17 @@ public class SignInDialog extends DialogFragment {
         builder.setTitle("Login");
         builder.setView(view);
 
-        final EditText emailText = view.findViewById(R.id.email);
-        final EditText passwordText = view.findViewById(R.id.password);
+        emailText = view.findViewById(R.id.email);
+        passwordText = view.findViewById(R.id.password);
+        textView = view.findViewById(R.id.textView);
+
 
         builder.setNegativeButton("Cancel", null);
         builder.setPositiveButton("Sign in", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                Toast.makeText(getActivity(),emailText.getText().toString() + passwordText.getText().toString(), Toast.LENGTH_SHORT).show();
+//          LOG IN!
+
             }
         });
 
